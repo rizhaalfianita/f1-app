8075ff65df10028b4258d444f851309f82ee060b
Fahrendra Khoirul <fahrendra.khoirul@gmail.com>
2023-10-29 13:56:20 +0700

    get init

---
+++ lib/app/modules/home/views/splash_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+class SplashView extends GetView {
+  const SplashView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SplashView'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'SplashView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
