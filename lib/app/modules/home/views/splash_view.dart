58def5557aad6a8338d58ef9eafc614d79b472ed
Fahrendra Khoirul <fahrendra.khoirul@gmail.com>
2023-10-29 14:44:23 +0700

    splash done

--- lib/app/modules/home/views/splash_view.dart
+++ lib/app/modules/home/views/splash_view.dart
@@ -1,20 +1,29 @@
+import 'package:f1_app/constant.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
 
 class SplashView extends GetView {
   const SplashView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    // Delay for 5 seconds and then navigate to the home screen
+    Future.delayed(Duration(seconds: 5), () {
+      Get.offNamed(
+          '/home'); // Navigate to the home screen using GetX navigation
+    });
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SplashView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Shimmer(
+          duration: const Duration(seconds: 5),
+          colorOpacity: 0.5,
+          child: Image.asset(
+            "assets/images/f1_logo.png",
+            width: 200,
+          ),
         ),
       ),
     );
---
+++ lib/constant.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color f1RedColor = const Color(0xFFFF1801);
