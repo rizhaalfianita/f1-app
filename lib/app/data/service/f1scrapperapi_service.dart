a11ed2342a1bc99b59c9f472cd742062cfd3971a
Fahrendra Khoirul <fahrendra.khoirul@gmail.com>
2023-10-31 22:50:40 +0700

    add f1upcoming model service

--- lib/app/data/service/f1scrapperapi_service.dart
+++ lib/app/data/service/f1scrapperapi_service.dart
@@ -1,19 +1,22 @@
 // create F1 api service
 
 import 'package:dio/dio.dart';
-import 'package:f1_app/app/data/model/F1AllSeason_model.dart';
+import 'package:f1_app/app/data/model/f1allseason_model.dart';
 import 'package:f1_app/app/data/model/f1season_model.dart';
+import 'package:f1_app/app/data/model/f1upcoming_model.dart';
 
 class F1Service {
-  static var baseUrl = "https://f1-scrapper-api.onrender.com/";
+  static var baseUrl = "https://f1scrapperapi.onrender.com/";
   static var seasonUrl = "${baseUrl}season/";
-  static var allSeasonUrl = "${baseUrl}all-seasons/";
+  static var allSeasonUrl = "https://f1scrapperapi.onrender.com/all-season";
+  static var upcomingUrl = "https://f1scrapperapi.onrender.com/upcoming";
   static var dio = Dio();
 
   static Future<List<F1AllSeason>?> getAllSeason() async {
     var res = await dio.get(allSeasonUrl);
     if (res.statusCode == 200) {
       var allSeasons = res.data as List;
+      print("F1 SERVICE : $allSeasons");
       return allSeasons.map((e) => F1AllSeason.fromJson(e)).toList();
     } else {
       return null;
@@ -25,6 +28,16 @@ class F1Service {
     if (res.statusCode == 200) {
       var season = res.data as List;
       return season.map((e) => F1Season.fromJson(e)).toList();
+    } else {
+      return null;
+    }
+  }
+
+  static Future<F1Upcoming?> getUpcoming() async {
+    var res = await dio.get(upcomingUrl);
+    if (res.statusCode == 200) {
+      var upcoming = res.data;
+      return F1Upcoming.fromJson(upcoming);
     } else {
       return null;
     }
